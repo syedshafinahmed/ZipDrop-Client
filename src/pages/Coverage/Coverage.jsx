8e105bfd5939d070d9cdde86eb58f4beb09fbdee
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-11-16 19:45:13 +0600

    implemented map integration with leaflet

--- src/pages/Coverage/Coverage.jsx
+++ src/pages/Coverage/Coverage.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
-
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { useLoaderData } from 'react-router';
 const Coverage = () => {
+  const serviceCenters = useLoaderData();
+  const position = [23.6850, 90.3563];
   return (
     <div className='py-20'>
       <h2 className='text-secondary text-3xl font-bold'>We are available in 64 districts</h2>
+      <h2 className='text-secondary text-xl font-bold'>We deliver almost all over Bangladesh</h2>
+      <div className='border border-secondary w-full h-[550px]'>
+        <MapContainer className='h-[550px]'
+          center={position}
+          zoom={7}
+          scrollWheelZoom={false}
+          attributionControl={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {
+            serviceCenters.map((center, index) => (<Marker key={index} position={[center.latitude, center.longitude]}>
+              <Popup>
+                <strong>{center.district}</strong> <br />Service Area: {center.covered_area.join(', ')}
+              </Popup>
+            </Marker>))
+          }
+        </MapContainer>
+      </div>
     </div>
   );
 };
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -14,7 +14,8 @@ export const router = createBrowserRouter([
       },
       {
         path: '/coverage',
-        Component: Coverage
+        Component: Coverage,
+        loader: () => fetch('/serviceCenters.json').then(res => res.json())
       }
     ]
   },
