dff6a85c457e5f674e1043c75a46a73ad7ac3c77
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-11-16 20:13:51 +0600

    implemented search location

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from '../../assets/logo.png'
+import { NavLink } from 'react-router';
 const Logo = () => {
   return (
-    <div className='flex items-end'>
-      <img src={logo} alt="" />
-      <h3 className='text-3xl font-bold -ms-2.5'>ZapShift</h3>
-    </div>
+    <NavLink to='/'>
+      <div className='flex items-end'>
+        <img src={logo} alt="" />
+        <h3 className='text-3xl font-bold -ms-2.5'>ZapShift</h3>
+      </div>
+    </NavLink>
   );
 };
 
--- src/pages/Coverage/Coverage.jsx
+++ src/pages/Coverage/Coverage.jsx
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { useLoaderData } from 'react-router';
 const Coverage = () => {
   const serviceCenters = useLoaderData();
+  const mapRef = useRef(null);
+  const handleSearch = e => {
+    e.preventDefault();
+    const location = e.target.location.value;
+    const district = serviceCenters.find(c => c.district.toLowerCase().includes(location.toLowerCase()));
+    if (district) {
+      const coord = [district.latitude, district.longitude];
+      mapRef.current.flyTo(coord, 12);
+    }
+  }
   const position = [23.6850, 90.3563];
   return (
     <div className='py-20'>
       <h2 className='text-secondary text-3xl font-bold'>We are available in 64 districts</h2>
-      <h2 className='text-secondary text-xl font-bold'>We deliver almost all over Bangladesh</h2>
-      <div className='border border-secondary w-full h-[550px]'>
-        <MapContainer className='h-[550px]'
+      <form onSubmit={handleSearch} className="relative w-full md:w-1/3 py-10">
+        <input
+          type="text"
+          name='location'
+          placeholder="Search here..."
+          className="input border border-secondary h-12 outline-none input-bordered w-full rounded-xl pr-32"
+        />
+        <button
+          className="absolute top-1/2 right-1 btn -translate-y-1/2 z-10 bg-primary text-black text-xs font-bold px-6 py-1 rounded-xl "
+        >
+          Search
+        </button>
+      </form>
+      <h2 className='text-secondary text-xl font-bold pb-5'>We deliver almost all over Bangladesh</h2>
+      <div className='rounded-xl w-full h-96'>
+        <MapContainer className='h-96 rounded-xl'
           center={position}
           zoom={7}
           scrollWheelZoom={false}
           attributionControl={false}
+          ref={mapRef}
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
