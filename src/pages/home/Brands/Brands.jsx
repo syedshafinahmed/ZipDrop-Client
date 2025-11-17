38c696cc96b09eafe64215e7dae1581c0a40a12c
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-11-18 00:55:26 +0600

    updated brands section

--- src/pages/home/Brands/Brands.jsx
+++ src/pages/home/Brands/Brands.jsx
@@ -8,6 +8,14 @@ import img4 from '../../../assets/brands/moonstar.png'
 import img5 from '../../../assets/brands/randstad.png'
 import img6 from '../../../assets/brands/star.png'
 import img7 from '../../../assets/brands/start_people.png'
+import img8 from '../../../assets/brands/DHL.png'
+import img9 from '../../../assets/brands/Aramex.png'
+import img10 from '../../../assets/brands/maersk.png'
+import img11 from '../../../assets/brands/ups.png'
+import img12 from '../../../assets/brands/fedex.png'
+import img13 from '../../../assets/brands/Ecourier.png'
+import img14 from '../../../assets/brands/redx.png'
+import img15 from '../../../assets/brands/pathao.png'
 import { Autoplay } from 'swiper/modules';
 const Brands = () => {
   return (
@@ -31,7 +39,15 @@ const Brands = () => {
         <SwiperSlide><img src={img4} alt="" /></SwiperSlide>
         <SwiperSlide><img src={img5} alt="" /></SwiperSlide>
         <SwiperSlide><img src={img6} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={img15} alt="" /></SwiperSlide>
         <SwiperSlide><img src={img7} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={img8} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={img9} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={img10} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={img11} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={img12} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={img13} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={img14} alt="" /></SwiperSlide>
       </Swiper>
     </div>
   );
