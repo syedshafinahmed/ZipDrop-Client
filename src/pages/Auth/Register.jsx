993cc6f04b41aa99e928c91d8c5d1fdae7ffc3e3
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-11-17 12:24:32 +0600

    react hoook form

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Register = () => {
+  const { register, formState: { errors }, handleSubmit } = useForm();
+  const handleRegistration = () => {
+
+  }
   return (
     <div>
-      register
+      <form onSubmit={handleSubmit(handleRegistration)}>
+        <fieldset className="fieldset">
+          <label className="label">Email</label>
+          <input type="email" {...register('email', { required: true })} className="input" placeholder="Email" />
+          {errors.email?.type === 'required' && <p className='text-red-500'>Email is required</p>}
+          <label className="label">Password</label>
+          <input type="password" {...register('password', { required: true, minLength: 6 })} className="input" placeholder="Password" />
+          {errors.password?.type === 'required' && <p className='text-red-500'>Password is required</p>}
+          {errors.password?.type === 'minLength' && <p className='text-red-500'>Password must be atleast 6 characters long.</p>}
+          <div><a className="link link-hover">Forgot password?</a></div>
+          <button className="btn btn-neutral mt-4">Login</button>
+        </fieldset>
+      </form>
     </div>
   );
 };
