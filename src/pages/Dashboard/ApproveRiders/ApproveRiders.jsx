29f01a27d73ed2b90fd85de6bd1f08bbb2a373f1
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-11-23 21:51:05 +0600

    Approve or reject rider and set rider role

--- src/pages/Dashboard/ApproveRiders/ApproveRiders.jsx
+++ src/pages/Dashboard/ApproveRiders/ApproveRiders.jsx
@@ -4,10 +4,11 @@ import useAxiosSecure from '../../../hooks/useAxiosSecure';
 import { FaTrash } from 'react-icons/fa';
 import { SiTicktick } from 'react-icons/si';
 import { ImCross } from 'react-icons/im';
+import Swal from 'sweetalert2';
 
 const ApproveRiders = () => {
   const axiosSecure = useAxiosSecure();
-  const { data: riders = [] } = useQuery({
+  const { refetch, data: riders = [] } = useQuery({
     queryKey: ['riders', 'pending'],
     queryFn: async () => {
       const res = await axiosSecure.get('/riders');
@@ -16,8 +17,30 @@ const ApproveRiders = () => {
     }
   })
 
-  const handleApproval = (id) => {
-    
+  const updateRiderStatus = (rider, status) => {
+    const updateInfo = { status: status, email: rider.email };
+    axiosSecure.patch(`/riders/${rider._id}`, updateInfo)
+      .then(res => {
+        if (res.data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Rider Application has been approved",
+            showConfirmButton: false,
+            timer: 2000,
+            text: "Congratulations! You're a rider now.",
+          });
+        }
+      })
+  }
+
+  const handleApproval = (rider) => {
+    updateRiderStatus(rider, 'approved');
+  }
+
+  const handleRejection = (rider) => {
+    updateRiderStatus(rider, 'rejected');
   }
 
 
@@ -31,7 +54,7 @@ const ApproveRiders = () => {
             <thead>
               <tr>
                 <th className="border border-gray-300 text-center">Sl</th>
-                <th className="border border-gray-300 text-center">Name</th>
+                <th className="border border-gray-300 text-center wrap-break-word whitespace-normal">Name</th>
                 <th className="border border-gray-300 text-center">Driving Lisence</th>
                 <th className="border border-gray-300 text-center">Region</th>
                 <th className="border border-gray-300 text-center">District</th>
@@ -45,20 +68,25 @@ const ApproveRiders = () => {
               {riders.map((rider, index) => (
                 <tr key={rider._id}>
                   <th className="border border-gray-300 text-center">{index + 1}</th>
-                  <td className="border border-gray-300 text-center">{rider.name}</td>
+                  <td className="border border-gray-300 text-center wrap-break-word whitespace-normal">{rider.name}</td>
                   <td className="border border-gray-300 text-center">{rider.drivingLicense}</td>
                   <td className="border border-gray-300 text-center">{rider.region}</td>
                   <td className="border border-gray-300 text-center">{rider.district}</td>
                   <td className="border border-gray-300 text-center">{rider.bike}</td>
-                  <td className="border border-gray-300 text-center">{rider.status}</td>
-                  <td className="text-center flex items-center justify-between gap-3">
-                    <button onClick={() => handleApproval(rider._id)} className="btn hover:bg-primary">
+                  <td className="border border-gray-300 text-center">
+                    <p
+                      className={rider.status === 'approved' ? 'badge badge-secondary' : 'badge badge-error'}>
+                      {rider.status}
+                    </p>
+                  </td>
+                  <td className="text-center flex items-center">
+                    <button onClick={() => handleApproval(rider)} className="btn btn-square hover:bg-primary">
                       <SiTicktick />
                     </button>
-                    <button className="btn hover:bg-primary">
+                    <button onClick={() => handleRejection(rider)} className="btn btn-square hover:bg-primary">
                       <ImCross />
                     </button>
-                    <button className="btn hover:bg-primary">
+                    <button className="btn btn-square hover:bg-primary">
                       <FaTrash />
                     </button>
                   </td>
