b0ef43aa54c2ed2d390d47e190d4e159c963baff
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-11-28 17:32:32 +0600

    Toggle user Role Make Admin and Remove from admin

--- src/pages/Dashboard/UsersManagement/UsersManagement.jsx
+++ src/pages/Dashboard/UsersManagement/UsersManagement.jsx
@@ -1,38 +1,78 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import { FaUserShield } from "react-icons/fa6";
+import { FiShieldOff } from "react-icons/fi";
+import Swal from 'sweetalert2';
 
 const UsersManagement = () => {
   const axiosSecure = useAxiosSecure();
-  const { data: users = [] } = useQuery({
+  const { refetch, data: users = [] } = useQuery({
     queryKey: ['users'],
     queryFn: async () => {
       const res = await axiosSecure.get(`/users`);
       return res.data;
     }
   })
+
+  const handleMakeUser = (user) => {
+    const roleInfo = { role: 'admin' }
+    axiosSecure.patch(`/users/${user._id}`, roleInfo)
+      .then(res => {
+        if (res.data.modifiedCount) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${user.displayName} marked as an Admin`,
+            showConfirmButton: false,
+            timer: 2000
+          }).then(() => refetch())
+        }
+      })
+  }
+
+  const handleRemoveAdmin = (user) => {
+    const roleInfo = { role: 'user' }
+    axiosSecure.patch(`/users/${user._id}`, roleInfo)
+      .then(res => {
+        if (res.data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${user.displayName} removed from Admin`,
+            showConfirmButton: false,
+            timer: 2000
+          })
+        }
+      })
+  }
+
+
   return (
     <div>
       <h2 className='text-5xl text-center py-2'>Manage Users: {users.length}</h2>
-      <div className="overflow-x-auto">
+      <div className="overflow-x-auto px-20 mt-10">
         <table className="table">
           {/* head */}
           <thead>
             <tr>
-              <th>
+              <th className='text-center border border-gray-300'>
                 Sl
               </th>
-              <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
+              <th className='text-center border border-gray-300'>Name</th>
+              <th className='text-center border border-gray-300'>Email</th>
+              <th className='text-center border border-gray-300'>Role</th>
+              <th className='text-center border border-gray-300'>Admin Action</th>
+              <th className='text-center border border-gray-300'>Actions</th>
             </tr>
           </thead>
           <tbody>
             {users.map((user, index) => <tr>
-              <td>
+              <td className='text-center border border-gray-300'>
                 {index + 1}
               </td>
-              <td>
+              <td className='text-center border border-gray-300'>
                 <div className="flex items-center gap-3">
                   <div className="avatar">
                     <div className="mask mask-squircle h-12 w-12">
@@ -47,13 +87,24 @@ const UsersManagement = () => {
                   </div>
                 </div>
               </td>
-              <td>
+              <td className='text-center border border-gray-300'>
                 {user.email}
               </td>
-              <td>Admin</td>
-              <th>
+              <td className='text-center border border-gray-300'>
+                {user.role}
+              </td>
+              <td className='text-center border border-gray-300'>
+                {user.role === 'admin' ?
+                  <button onClick={() => handleRemoveAdmin(user)} className='btn bg-red-500'>
+                    <FiShieldOff />
+                  </button> :
+                  <button onClick={() => handleMakeUser(user)} className='btn bg-primary'>
+                    <FaUserShield />
+                  </button>}
+              </td>
+              <td className='text-center border border-gray-300'>
                 Actions
-              </th>
+              </td>
             </tr>)}
 
           </tbody>
